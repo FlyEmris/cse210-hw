931fb1a2fcd8ad25a90e8613af4fc9ebbab323a4
Jonathan <jonnyislenny@gmail.com>
2023-09-27 17:44:43 -0600

    resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        job job1 = new Job();
+        job1._title = 'Lifeguard';
+        job1._company = 'Ymca';
+        job1._yearStart = 2015;
+        job1._yearEnd = 2017;
+
+        job job2 = new Job():
+        job2._title = 'construction';
+        job2._company = 'J&M Construction';
+        job2._yearStart = 2021;
+        job2._yearStart = 2023;
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Jonathan Leonardson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
