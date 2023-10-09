fd69b37a6f13066b5ffcc13f4c73a8fb66b6f7aa
Jonathan <jonnyislenny@gmail.com>
2023-10-09 09:40:11 -0600

    finished learning 3 activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f = new Fraction();
+        Console.WriteLine(f.GetFractionString());
+        Console.WriteLine(f.GetDecimalValue());
+
+        Fraction w = new Fraction(5);
+        Console.WriteLine(w.GetFractionString());
+        Console.WriteLine(w.GetDecimalValue());
+
+        Fraction s1 = new Fraction(3, 4);
+        Console.WriteLine(s1.GetFractionString());
+        Console.WriteLine(s1.GetDecimalValue());
+
+        Fraction s2 = new Fraction(1, 3);
+        Console.WriteLine(s2.GetFractionString());
+        Console.WriteLine(s2.GetDecimalValue());
     }
 }
+    
