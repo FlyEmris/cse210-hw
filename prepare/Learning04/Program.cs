9b21b8b4ab86f033b98463fde9158f40085200d5
Jonathan <jonnyislenny@gmail.com>
2023-10-24 17:02:50 -0600

    learning activity 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assig1 = new Assignment();
+        assig1.SetStudentName("Samuel Bennett");
+        assig1.SetTopic("Multiplication");
+
+        Console.WriteLine(assig1.GetSummery());
+
+        MathAssignment math = new MathAssignment();
+        math.SetStudentName("Roberto Rodriguez");
+        math.SetTopic("Fractions");
+        math.SetTextBookSection("Section 7.3");
+        math.SetProblems("Problems 8-19");
+
+        Console.WriteLine(math.GetSummery());
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment write = new WritingAssignment();
+        write.SetTitle("The Causes of World War II");
+        write.SetStudentName("Mary Waters");
+        write.SetTopic("European History");
+
+        Console.WriteLine(write.GetSummery());
+        Console.WriteLine(write.GetWritingInformation());
     }
 }
